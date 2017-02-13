4180fb00d0083ad763574476d0bfb22528eb70e5
岡実来 <MIRAI@okamirai-MacBook-Pro.local>
2017-02-13 20:44:09 +0900

    add Reload function

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -9,17 +9,26 @@ public class GunController : MonoBehaviour {
 	int bulletBoxCount = 150;
 	public GameObject gunEffect;
 	AudioSource audioBullet;
-	public AudioClip audioClip;
+	public AudioClip audioBulletClip;
+	AudioSource audioReload;
+	public AudioClip audioReloadClip;
+	bool isRoad;
 	// Use this for initialization
 	void Start () {
 		audioBullet = GetComponent<AudioSource> ();
+		audioReload = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		bulletInterval += Time.deltaTime;
-		if(Input.GetMouseButton(0) && bulletInterval > 0.1f && bulletCount > 0){
+
+		if(Input.GetMouseButton(0) && bulletInterval > 0.1f && bulletCount > 0 && !(isRoad)){
 			GenerateBullet ();
+		}
+
+		if (Input.GetKey (KeyCode.R) && bulletCount < 30 && bulletBoxCount > 0) {
+			StartCoroutine ("Reload");
 		}
 
 	}
@@ -27,7 +36,7 @@ public class GunController : MonoBehaviour {
 	void GenerateBullet(){
 		bulletCount--;
 		Ray ray = new Ray(camera.transform.position, camera.transform.forward);
-		audioBullet.PlayOneShot (audioClip);
+		audioBullet.PlayOneShot (audioBulletClip);
 		RaycastHit hit = new RaycastHit();
 		Instantiate (gunEffect, transform.position + ray.direction * 0.9f + new Vector3(0,0.1f,0), Quaternion.identity);
 		if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
@@ -35,4 +44,20 @@ public class GunController : MonoBehaviour {
 		}
 		bulletInterval = 0.0f;
 	}
+
+	IEnumerator Reload(){
+		int addBullet = (30 - bulletCount);//増やす予定の弾数
+		if (bulletBoxCount < addBullet) {
+			bulletCount += bulletBoxCount;
+			bulletBoxCount = 0;
+		} else {
+			bulletCount = 30;
+			bulletBoxCount -= addBullet;
+		}
+		audioBullet.PlayOneShot (audioReloadClip);
+		isRoad = true;
+		yield return new WaitForSeconds (2.5f);
+		isRoad = false;
+	}
+
 }
